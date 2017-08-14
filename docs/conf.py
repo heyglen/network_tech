ee53a6fd48197be68a30329f3327e5e0755f25d0
Glen Harmon <glencharmon@gmail.com>
2017-08-14 16:10:06 +0200

    version bump

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ author = 'Glen Harmon'
 # built documents.
 #
 # The short X.Y version.
-version = '2.1.0'
+version = '2.2.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
