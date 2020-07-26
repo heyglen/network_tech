d42fbc1fc676bac04876543cfed182933b75642a
Glen Harmon <glencharmon@gmail.com>
2020-07-26 11:14:46 +0200

    2.47.0 -> 2.48.0

--- lib/passwords/pw_type7.py
+++ lib/passwords/pw_type7.py
@@ -1,18 +1,48 @@
 # Copyright 2017 Glen Harmon
 
 import re
+import itertools
 
-password_re = re.compile(r'^(?P<salt_index>[01]\d)(?P<encrypted_text>[0-9a-f]{1,25})', re.I)
+ios_password_re = re.compile(r'^(?P<salt_index>[01]\d)(?P<encrypted_text>\S{1,25})', re.I)
 
 
 TRANSLATION_KEY_MAP = 'dsfd;kfoA,.iyewrkldJKDHSUBsgvca69834ncxv9873254k;fg87'
 
-# Following https://pen-testing.sans.org/resources/papers/gcih/cisco-ios-type-7-password-vulnerability-100566
 
 def decode(password):
+    if ios_password_re.match(password):
+        return ios_decode(password)
+    else:
+        return nxos_decode(password)
+
+
+def nxos_decode(password):
+    """ See https://networkengineering.stackexchange.com/questions/27987/tacacs-implentation-server-key-error """
+    uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    lowercase = uppercase.lower()
+    decoded_password = list()
+    cipher = [3, 22, 4, 5, 18, 0, 21, 5, 18, 3, 10, 5, 16, 22, 4, 16, 24, 17, 12, 5, 21, 18, 5, 22, 19, 7]
+    for character, key in zip(password, itertools.cycle(cipher)):
+        if character in lowercase:
+            decoded = ord(character) - 97 - key
+            if decoded < 0:
+                decoded += 26
+            decoded += 97
+            decoded_password.append(chr(decoded))
+        elif character in uppercase:
+            decoded = ord(character) - 65 - key
+            if decoded < 0:
+                decoded += 26
+            decoded += 65
+            decoded_password.append(chr(decoded))
+        else:
+            decoded_password.append(character)
+    return ''.join(decoded_password)
+
+
+def ios_decode(password):
+    """ Following https://pen-testing.sans.org/resources/papers/gcih/cisco-ios-type-7-password-vulnerability-100566 """
     decoded_password = ''
-    if not password_re.match(password):
-        return 'Invalid Type 7 Password'
 
     # Example Password:
     #   Encoded: 044B0A151C36435C0D
